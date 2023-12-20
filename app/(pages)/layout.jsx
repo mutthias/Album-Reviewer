2b272c5b893b624eae09a6473d25c371686a0712
Austin Shih <austinhshih@gmail.com>
2023-12-19 19:55:43 -0800

    modularized fonts and metadata

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -1,26 +1,17 @@
-import { Inter } from 'next/font/google';
 import '@globals/globals.scss';
+import fonts from '@globals/fonts';
+import metadata from '@globals/metadata';
 
 import navLinks from '@data/navLinks.json';
 import Navbar from '@components/Navbar/Navbar';
 import Footer from './_components/Footer/Footer';
 
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-});
-
-export const metadata = {
-  title: 'Next.js Starter Template',
-  icons: {
-    icon: '/icons/icon.ico',
-  },
-};
+export { metadata };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable}`}>
+      <body className={fonts}>
         <Navbar navLinks={navLinks} />
         {children}
         <Footer navLinks={navLinks} />
